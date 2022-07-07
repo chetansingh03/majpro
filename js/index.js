87b9789f49da4c10b4bff88719f53c1b0d48e3ec
Chetan Singh <97108649+chetansingh03@users.noreply.github.com>
2022-07-07 16:29:23 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,42 @@
+"use strict";
+
+function clearError() {
+    errors = document.getElementsByClassName("formerror");
+    for (let item of errors) {
+        item.innerHTML = "";
+    }
+}
+
+function seterror(id, error) {
+    //sets error inside tag of id
+    element = document.getElementById(id);
+    element.getElementsByClassName("formerror")[0].innerHTML = error;
+}
+
+function validateForm() {
+    var returnval = true;
+    clearError();
+
+    //perform validation and if validation fails, set the value of returnval to false
+    var name = document.forms["myForm"]["fname"].value;
+    if (name.length < 4) {
+        seterror("name", "*Name is not valid!");
+        returnval = false;
+    }
+    if (name.length > 20) {
+        seterror("name", "*Name is not valid!");
+        returnval = false;
+    }
+
+    var password = document.forms["myForm"]["fpass"].value;
+
+    if (password.length < 6) {
+        seterror("pass", "*Password is not valid!");
+        returnval = false;
+    }
+    if (password.length > 25) {
+        seterror("pass", "*Password is not valid!");
+        returnval = false;
+    }
+    return returnval;
+}
---
+++ js/main2.js
@@ -0,0 +1,52 @@
+function clearErrors() {
+    errors = document.getElementsByClassName("formerror");
+    for (let item of errors) {
+        item.innerHTML = "";
+    }
+}
+
+function seterror(id, error) {
+    //sets error inside tag of id
+    element = document.getElementById(id);
+    element.getElementsByClassName("formerror")[0].innerHTML = error;
+}
+
+function validateForm() {
+    var returnval = true;
+    clearErrors();
+
+    //perform validation and if validation fails, set the value of returnval to false
+    var name = document.forms["myForm"]["fname"].value;
+    if (name.length < 4) {
+        seterror("name", "*Length of name is too short!");
+        returnval = false;
+    }
+    if (name.length > 15) {
+        seterror("name", "*Length of name is too long!");
+        returnval = false;
+    }
+
+    var email = document.forms["myForm"]["femail"].value;
+    if (email.length > 40) {
+        seterror("email", "*Email length is too long!");
+        returnval = false;
+    }
+
+    var password = document.forms["myForm"]["fpass"].value;
+    if (password.length < 6) {
+        seterror("pass", "*Password should be atleast 6 characters long!");
+        returnval = false;
+    }
+    if (password.length > 25) {
+        seterror("pass", "*Password should be atmost 25 characters long!");
+        returnval = false;
+    }
+
+    var cpassword = document.forms["myForm"]["fconfirm"].value;
+    if (cpassword != password) {
+        seterror("confirm", "*Password and Confirm password should be same!");
+        returnval = false;
+    }
+
+    return returnval;
+}
